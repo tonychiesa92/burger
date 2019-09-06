d7b9d4e1717048ac066b28ccf3f6e027db7add68
Tony C <tonychiesa92@gmail.com>
2019-09-06 04:38:47 -0400

    place order button will add new burgers to the list but devour it button is not working.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var burgers = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
@@ -21,7 +21,7 @@ router.get("/burgers", function (req, res) {
 });
 
 router.post("/burgers/create", function (req, res) {
-    burger.insertOne([
+    burgers.insertOne([
         "burger_name"
     ], [
             req.body.burger_name
@@ -36,7 +36,7 @@ router.put("/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    burger.updateOne({
+    burgers.updateOne({
         "devoured": req.body.devoured
     }, condition, function (result) {
         res.redirect("/burgers")
