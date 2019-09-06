89957b8bd425eb0afbd62ab8d7a7bfe500992f42
Tony C <tonychiesa92@gmail.com>
2019-09-06 02:49:19 -0400

    Set up schema and seeds to create and populate burgers table within burger_db.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
