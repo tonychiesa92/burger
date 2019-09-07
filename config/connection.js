7101dc137a99086143d14afb239709e767bdd753
Tony C <tonychiesa92@gmail.com>
2019-09-07 00:32:47 -0400

    updated connection for heroku.

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,43 @@
+// var mysql = require('mysql');
+
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: 'password1',
+//   database: 'burger_db'
+// });
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+// Node Dependency
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password1',
-  database: 'burger_db'
-});
+// For Heroku Deployment vs. Local MySQL Database
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: 'password1', // Add your password
+    database: 'burgers_db' // Add your database
+  });
+}
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
-// Export connection for our ORM to use.
+// Export the Connection
 module.exports = connection;
-
